55d7ad301ae39b2dc12d4bbb338e21fb28121224
Mike Lange <Mike.Lange@jda.com>
2018-02-08 13:39:09 -0600

    First commit to GitHub

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Value: %d\n", '');
+    printf("String: [%s]\n", "");
+
+    exit(0);
+}
+
---
+++ makefile
@@ -0,0 +1,7 @@
+main: main.o
+	$(CC) main.o -lc -o main
+
+clean:
+	-@rm -f main
+	-@rm -f main.o
+
